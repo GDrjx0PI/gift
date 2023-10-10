fea90cd19f6a378b020c58e47c85c4bff8c27511
GDrjx0PI <147529573+GDrjx0PI@users.noreply.github.com>
2023-10-11 00:32:40 +0900

    Add files via upload

--- draw.py
+++ draw.py
@@ -14,11 +14,11 @@ def main():
     if (st.session_state.red_flag == "0") and (st.session_state.blue_flag == "0"):
         st.title("誕生日おめでとう!")
         st.write("赤い封筒と青い封筒のどっちか選んで!")
-        red_image = Image.open('red.emf')
+        red_image = Image.open('red.png')
 
         st.image(red_image, use_column_width=True)
         red_btn = st.button('赤い封筒にする')
-        blue_image = Image.open('blue.emf')
+        blue_image = Image.open('blue.png')
         if red_btn:
             st.session_state.red_flag = "1"
             write(file_path="red.cfg", content="1")
