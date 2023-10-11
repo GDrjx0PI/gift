181133fc5b3adffa8fdfda5875f61ee3df827c3b
GDrjx0PI <147529573+GDrjx0PI@users.noreply.github.com>
2023-10-11 20:27:52 +0900

    Add files via upload

--- draw.py
+++ draw.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_autorefresh import st_autorefresh
 from PIL import Image
 
 
@@ -14,25 +15,22 @@ def main():
     if (st.session_state.red_flag == "0") and (st.session_state.blue_flag == "0"):
         st.title("誕生日おめでとう!")
         st.write("赤い封筒と青い封筒のどっちか選んで!")
-        red_image = Image.open('red.emf')
 
+        red_image = Image.open('red.png')
         st.image(red_image, use_column_width=True)
         red_btn = st.button('赤い封筒にする')
-        blue_image = Image.open('blue.emf')
         if red_btn:
             st.session_state.red_flag = "1"
             write(file_path="red.cfg", content="1")
-            st.button("ここを押してください")
+            st_autorefresh(interval=100, limit=100)
 
+        blue_image = Image.open('blue.png')
         st.image(blue_image, use_column_width=True)
         blue_btn = st.button('青い封筒にする')
         if blue_btn:
             st.session_state.blue_flag = "1"
             write(file_path="blue.cfg", content="1")
-            st.button("ここを押してください")
-        
-        # if red_btn or blue_btn:
-        #     st.button("ここを押してください")
+            st_autorefresh(interval=100, limit=100)
         
         print("1", st.session_state.red_flag, st.session_state.blue_flag)
     else:
